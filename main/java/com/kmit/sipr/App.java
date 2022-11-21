add5fbbe1d2563acf3ce85c53430b8c88fbec6b2
SripathiMeghana23 <meghanasripathi23@gmail.com>
2022-11-21 20:01:17 +0530

    Demo for team

---
+++ main/java/com/kmit/sipr/App.java
@@ -0,0 +1,13 @@
+package com.kmit.sipr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
